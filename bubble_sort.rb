916f3995c7b544b0d5faa5f4a0d8f170a984bb50
Ron Javellana <Ronapj1991@gmail.com>
2023-01-07 20:48:05 -0500

    create bubble sort method combining 2 methods

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,21 +11,76 @@ compare [1 and 2], sort the 2 values
 and so on till the end of array...
 =end
 
-given_array = [4,3,78,2,0,2]
-index_for_A = 0
-index_for_B = 1
-
-while (index_for_B < given_array.length)
-    a = given_array[index_for_A]
-    b = given_array[index_for_B]
-
-    if ((a - b) >= 0)
-        given_array[index_for_A] = b;
-        given_array[index_for_B] = a;
+def bubble_sort(given_array)
+    until (array_is_sorted?(given_array))
+        given_array = single_pass_sort(given_array)
     end
 
-    index_for_A += 1
-    index_for_B += 1
+    given_array
 end
 
-p given_array
+def array_is_sorted?(arr)
+    is_sorted = []
+    index_for_A = 0
+    index_for_B = 1
+
+    while(index_for_B < arr.length)
+        a = arr[index_for_A]
+        b = arr[index_for_B]
+
+        a <= b ? is_sorted.push("sorted") : is_sorted.push("not sorted")
+
+        index_for_A += 1
+        index_for_B += 1
+    end
+
+    is_sorted.all?("sorted")
+end
+
+def single_pass_sort(arr)
+    index_for_A = 0
+    index_for_B = 1
+
+    while (index_for_B < arr.length)
+        a = arr[index_for_A]
+        b = arr[index_for_B]
+    
+        if ((a - b) >= 0)
+            arr[index_for_A] = b;
+            arr[index_for_B] = a;
+        end
+    
+        index_for_A += 1
+        index_for_B += 1
+    end
+
+    arr
+end
+
+
+def test(arr)
+    loop do
+        index_for_A = 0
+        index_for_B = 1
+    
+        while (index_for_B < arr.length)
+            a = arr[index_for_A]
+            b = arr[index_for_B]
+        
+            if ((a - b) >= 0)
+                arr[index_for_A] = b;
+                arr[index_for_B] = a;
+            end
+        
+            index_for_A += 1
+            index_for_B += 1
+        end
+        break if (array_is_sorted?(arr) == true)
+         
+        p arr
+
+        
+    end
+end
+
+p bubble_sort([4,3,78,2,0,2])
